f7a9258549533f90c039a3aae1eb6fada3d423ec
Hajime Todo <samsepylot@gmail.com>
2019-02-26 17:16:43 +0900

    環境構築

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./src/index.html"})
+    ]
+}
