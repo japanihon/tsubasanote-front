47c132b8a30eb3da7c158a75c487327230a0bbc6
Hajime Todo <samsepylot@gmail.com>
2019-03-04 22:20:42 +0900

    ラベル一覧を取得

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
-const Labels = () => (
-    <div>
-        <h2>Labels</h2>
-        <p>ラベル一覧</p>
-    </div>
-)
+class Labels extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            labels: []
+        };
+    }
+    componentDidMount() {
+        const request = axios.create({
+            baseURL: 'http://localhost:3000'
+        })
+        request.get('/labels')
+            .then(res => {
+                this.setState({
+                    labels:res.data
+                });
+            })
+    }
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <h2>Labels</h2>
+                <p>ラベル一覧</p>
+            </div>
+        )
+    }
+}
 
 export default Labels;
