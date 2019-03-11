1edb7a77f16bf60f348d454bd4d8e3a50e1dfc0d
tsubasa1122 <nakanot@open8.com>
2019-03-11 22:36:57 +0900

    サイドバー作成

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,12 +1,93 @@
 import React from 'react';
-import SideBar from './SideBar.js'
+import SideBar from './SideBar.js';
+import axios from 'axios';
+import styled from 'styled-components';
 
-const Notes = () => (
-    <div>
-        <SideBar />
-        <h2>Notes</h2>
-        <p>ノート一覧</p>
-    </div>
-)
+const Wrapper = styled.div` 
+    display: inline-block;
+    height: 100%;
+    width: 80%;
+`
+
+const PageTitle = styled.h1`
+    margin: 30px 0 30px 65px;
+    font-family: 'Arial';
+    font-size: 64px;
+    color: #888888;
+`
+const NoteUl = styled.ul`
+    list-style: none;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+`
+
+const NoteLi = styled.li`
+    height: 140px;
+    width: 100%;
+    border-top: 1px solid #AAAAAA;
+`
+const NoteTitle = styled.h2`
+    margin: 30px 0 0 65px;
+    font-family: 'Arial';
+    font-size: 18px;
+    font-weight: bold;
+    color: #888888;
+`
+const NoteDate = styled.p`
+    margin: 45px 0 0 65px;
+    font-family: 'Arial';
+    font-size: 16px;
+    color: #888888;
+`
+
+
+
+
+class Notes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: 0,
+            notes: []
+        };
+    }
+     componentDidMount() {
+         const request = axios.create({
+             baseURL: 'http://localhost:3000'
+         })
+         request.get('/notes')
+             .then(res => {
+                 this.setState({
+                     notes:res.data
+                 });
+             })
+     }
+    render() {
+         console.log(this.state);
+         const list =this.state.notes.map(note => {
+             return (
+                 <NoteLi className={note.id}>
+                     <NoteTitle>{note.title}</NoteTitle>
+                     <NoteDate>{note.created_at}</NoteDate>
+                 </NoteLi>
+             );
+         });
+
+        return (
+            <div>
+                <SideBar />
+                <Wrapper>
+                    <PageTitle>Note title</PageTitle>
+                    <NoteUl>
+                        {list}
+                    </NoteUl>
+                </Wrapper>
+            </div>
+        )
+    }
+}
+
+
 
 export default Notes;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,27 +1,91 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Sidebar = styled.div`
+  display: inline-block;
+  float: left;
+  height: 100%;
+  width: 20%;
+  background: #545973;
+  `
+const Icon = styled.div`
+  display: block;
+  border: none;
+  border-radius: 50%;
+  height: 64px;
+  width: 100%;
+  padding: 30px 0 42px 0;
+`
+const IconImg = styled.img`
+  float: left;
+  height: 64px;
+  width: 64px;
+  border-radius: 50%;
+  margin-left: 45px;
+  background: white;
+`
+
+const NavArea = styled.div`
+  height: 100%;
+  width: 100%;
+  text-align: center;
+`
+
+const CreateButton = styled.button`
+  height: 48px;
+  width: 195px;
+  font-family: 'Arial';
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 40px;
+  color: #525872;
+  background-color: #F7ECEB;
+`
+
+const UlTop = styled.ul`
+  list-style: none;
+  font-family: 'Arial';
+  font-size: 16px;
+  margin: 25px 0 475px 35px;
+` 
+
+const UlBottom = styled.ul`
+  list-style: none;
+  font-family: 'Arial';
+  font-size: 16px;
+  margin: 0 0 48px 35px;
+`
+
+const Li = styled.li`
+  margin: 15px 0;
+  color: #FFFFFF;
+`
+
 
 class SideBar extends React.Component{
-    constructor(props) {
-      super(props);
+    constructor() {
+      super();
     }
     render(){
       return(
-        <div className="sidebar">
-          <div className="sidebar-icon">
-            <img src="" alt=""/>
-          </div>
-          <button className="sidebar-button__create">Create note</button>
-          <ul className="sidebar-list__top">
-            <li>My services</li>
-            <li>Schedule</li>
-            <li>Completed</li>
-          </ul>
-          <ul className="sidebar-list__bottom">
-            <li>My account</li>
-            <li>Support</li>
-            <li>Sign out</li>
-          </ul>
-        </div>
+        <Sidebar>
+          <Icon>
+            <IconImg src="" alt=""/>
+          </Icon>
+          <NavArea>
+            <CreateButton>Create note</CreateButton>
+          </NavArea>
+            <UlTop>
+              <Li>My services</Li>
+              <Li>Schedule</Li>
+              <Li>Completed</Li>
+            </UlTop>
+            <UlBottom>
+              <Li>My account</Li>
+              <Li>Support</Li>
+              <Li>Sign out</Li>
+            </UlBottom>
+        </Sidebar>
       )
     };
 }
