e6af856cf9da29a4f61d769dc5d6e8c7108e795a
tsubasa1122 <nakanot@open8.com>
2019-03-19 13:06:33 +0900

    font-awesomeの導入

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import '@fortawesome/fontawesome-free/js/fontawesome';
 
 const rootEl = document.getElementById('app');
 
