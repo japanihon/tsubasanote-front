ab2412bc3f051996c84bf37d725fb31105e50d82
Hajime Todo <samsepylot@gmail.com>
2019-03-21 12:38:38 +0900

    ちょっと進めた

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -4,34 +4,39 @@ import styled from 'styled-components';
 import SideBar from './SideBar.js'
 
 const Wrapper = styled.div`
+  display: inline-block;
   width: 80%;
   height: 100%;
 `;
 const Content = styled.div``;
 const Label = styled.div`
-  width: 1000px;
-  height: 16px;
   display: flex;
+  padding: 0;
+  width: 100%;
+  height: 140px;
+  border-top: 1px solid #aaaaaa;
 `;
 const Title = styled.h1`
-  width: 100px;
-  height: 14px;
+  margin: 30px 0 30px 65px;
+  font-family: 'Arial';
+  font-size: 64px;
+  color: #888888;
 `;
 const Name = styled.div`
   width: 100px;
-  height: 14px;
+  /* height: 14px; */
 `;
 const Description = styled.div`
   width: 100px;
-  height: 14px;
+  /* height: 14px; */
 `;
 const Edit = styled.div`
   width: 100px;
-  height: 14px;
+  /* height: 14px; */
 `;
 const Delete = styled.div`
   width: 100px;
-  height: 14px;
+  /* height: 14px; */
 `;
 
 class Labels extends React.Component {
