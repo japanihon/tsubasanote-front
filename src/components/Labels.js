e56e7caf9db4d3ac1604298ecaddf343c4e7dc70
tsubasa1122 <nakanot@open8.com>
2019-03-25 23:30:29 +0900

    labelの作成実装

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -1,34 +1,114 @@
-import React from 'react';
-import axios from 'axios';
-import SideBar from './SideBar.js'
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { TwitterPicker } from "react-color";
+
+const Wrapper = styled.div`
+    display: inline-block;
+    width: calc(100vw - 260px);
+`;
+
+const TitleArea = styled.div`
+    display: block;
+    height: 19vh;
+    width: 100vw;
+`;
 
 class Labels extends React.Component {
     constructor() {
         super();
         this.state = {
-            labels: []
+            labels: [],
+            name: "",
+            description: "",
+            bg_color: "#ffffff",
+            visible_new_form: false
         };
     }
     componentDidMount() {
         const request = axios.create({
-            baseURL: 'http://localhost:3000'
-        })
-        request.get('/labels')
-            .then(res => {
-                this.setState({
-                    labels:res.data
-                });
-            })
+            baseURL: "http://localhost:3000"
+        });
+        request.get("/labels").then(res => {
+            this.setState({
+                labels: res.data
+            });
+        });
     }
+
+    addLabelForm = () => {
+        this.setState({ visible_new_form: !this.state.visible_new_form });
+    };
+
+    hideForm = () => {
+        this.setState({ visible_new_form: false });
+    };
+
+    handleNameChange = event => {
+        const name = event.target.value;
+        this.setState({ name });
+    };
+
+    handleDescriptionChange = event => {
+        const description = event.target.value;
+        this.setState({ description });
+    };
+
+    handleChangeComplete = color => {
+        this.setState({ bg_color: color.hex });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const request = axios.create({
+            baseURL: "http://localhost:3000"
+        });
+        request.post("/labels", {
+            name: this.state.name,
+            description: this.state.description,
+            bg_color: this.state.bg_color
+        });
+        alert("ノートが作成されました");
+    };
     render() {
-        console.log(this.state)
         return (
-            <div>
-                <SideBar />
-                <h2>Labels</h2>
-                <p>ラベル一覧</p>
-            </div>
-        )
+            <Wrapper>
+                <button onClick={this.addLabelForm}>new label</button>
+                {this.state.visible_new_form && (
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <span style={{ color: this.state.bg_color }}>
+                                Label preview
+                            </span>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                name="name"
+                                value={this.state.name}
+                                onChange={this.handleNameChange}
+                            />
+                            <input
+                                type="text"
+                                name="description"
+                                value={this.state.description}
+                                onChange={this.handleDescriptionChange}
+                            />
+                            <button onClick={this.hideForm}>Cancel</button>
+                            <input type="submit" value="Create label" />
+                        </div>
+                        <p style={{ color: this.state.bg_color }}>
+                            ラベル一覧
+                            <TwitterPicker
+                                color={this.state.bg_color}
+                                onChangeComplete={this.handleChangeComplete}
+                            />
+                        </p>
+                    </form>
+                )}
+                <TitleArea>Labels</TitleArea>
+            </Wrapper>
+        );
     }
 }
 
--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -2,6 +2,7 @@ import React from "react";
 import SideBar from "./SideBar.js";
 import axios from "axios";
 import styled from "styled-components";
+import { Z_BLOCK } from "zlib";
 
 const Wrapper = styled.div`
     display: inline-block;
@@ -9,16 +10,25 @@ const Wrapper = styled.div`
     width: 80%;
 `;
 const TitleArea = styled.div`
-    display: block;
+    display: inline-flex;
     width: 100%;
     border-bottom: 1px solid #aaaaaa;
 `;
 
 const NoteTitle = styled.h1`
     margin: 30px 0 30px 65px;
-    width: 95%;
+    width: 80vw;
     font-family: "Arial";
     font-size: 64px;
+    color: #888888;
+`;
+
+const NoteDate = styled.p`
+    margin-top: 75px;
+    margin-right: 20px;
+    display: inline-flex;
+    font-family: "Arial";
+    font-size: 16px;
     color: #888888;
 `;
 
@@ -42,7 +52,8 @@ class NoteDetail extends React.Component {
         super(props);
         this.state = {
             title: "",
-            content: ""
+            content: "",
+            created_at: ""
         };
     }
     componentDidMount() {
@@ -52,16 +63,31 @@ class NoteDetail extends React.Component {
         request.get(`/notes/${this.props.match.params.id}`).then(res => {
             this.setState({
                 title: res.data.title,
-                content: res.data.content
+                content: res.data.content,
+                created_at: res.data.created_at
             });
         });
     }
     render() {
+        const date = new Date(this.state.created_at);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
         return (
             <div>
                 <Wrapper>
                     <TitleArea>
                         <NoteTitle>{this.state.title}</NoteTitle>
+                        <NoteDate>
+                            <i
+                                className="fas fa-calendar-alt"
+                                style={{
+                                    color: "#AAAAAA",
+                                    marginRight: "15px"
+                                }}
+                            />
+                            {year}/{month}/{day}
+                        </NoteDate>
                     </TitleArea>
                     <NoteContentArea>
                         <NoteContent>{this.state.content}</NoteContent>
