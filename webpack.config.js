88092fe576280f6888127ae28fdc4f041ab06875
Hajime Todo <samsepylot@gmail.com>
2019-04-19 14:08:15 +0900

    画像を読み込めるように

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,42 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, "/dist"),
-        publicPath: '/',
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                      '@babel/preset-env',
-                      {
-                        plugins: [
-                          '@babel/plugin-proposal-class-properties'
-                        ]
-                      }
-                    ]
-                  },
-            }
-        }]
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.join(__dirname, 'dist'),
-        open: true,
-        hot: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: "./src/index.html"})
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              {
+                plugins: ["@babel/plugin-proposal-class-properties"]
+              }
+            ]
+          }
+        }
+      },
+      {
+        test: [/\.(jpg|jpeg|png|gif)/],
+        exclude: /node_modules/,
+        use: "url-loader"
+      }
     ]
-}
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "dist"),
+    open: true,
+    hot: true
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
+};
